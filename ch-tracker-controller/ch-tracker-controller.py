9af93cdabd76f27d74269bf802cb1e5b7bd2c14d
Andres Vahter <andres.vahter@gmail.com>
2014-06-06 18:24:17 +0300

    add live tracking to tracker-controller

--- ch-tracker-controller/ch-tracker-controller.py
+++ ch-tracker-controller/ch-tracker-controller.py
@@ -1,5 +1,6 @@
 import sys
 import time
+import datetime
 import argparse
 import struct
 import curses
@@ -68,8 +69,8 @@ def wait_ack():
         if time.time() - wait_ack_time > 0.02:
             return False, 'Error no ACK!'
 
-def set_position_with_keyboard(motor=0, move_degrees=1):
-    absolute = 0
+def set_position(motor=0, absolute=True, move_degrees=1.1):
+    move_degrees = move_degrees * 10 # scale with 10 to maintain floating point precision
     set_position_packet = struct.pack("<BBBh", CT_SET_POSITION, motor, absolute, move_degrees)
     send_packet(set_position_packet)
     result, msg = wait_ack()
@@ -169,10 +170,7 @@ try:
 
             msg = ""
             if pargs.position is not None:
-                is_absolute = 1
-                set_position_packet = struct.pack("<BBBh", CT_SET_POSITION, pargs.motor, is_absolute, pargs.position)
-                send_packet(set_position_packet)
-                result, msg = wait_ack()
+                result, msg = set_position(pargs.motor, absolute=True, move_degrees=pargs.position)
 
             elif pargs.enable is not None:
                 set_motor_state_packet = struct.pack("<BBB", CT_SET_MOTOR_STATE, pargs.motor, pargs.enable)
@@ -181,14 +179,29 @@ try:
 
             elif pargs.track:
                 stdscr.nodelay(1)
+                start = time.time()
                 while 1:
+                    #d = datetime.datetime(2014, 06, 06, 17, 43, 30)
+                    #t = time.mktime(d.timetuple()) + time.time() - start
+                    #tracker.set_epoch(t)
+
                     tracker.set_epoch(time.time())
                     az = tracker.azimuth()
                     elev = tracker.elevation()
 
                     stdscr.move(command_line_nr + 2, 0)
                     stdscr.clrtoeol()
-                    stdscr.addstr(command_line_nr + 2, text_offset, "azimuth %0.1f, elevation %0.1f, range %0.0f km" % (az, elev, tracker.range()/1000))
+                    stdscr.addstr(command_line_nr + 2, text_offset, "azimuth %0.2f, elevation %0.2f, range %0.0f km" % (az, elev, tracker.range()/1000))
+
+                    if az <= 180:
+                        az = az
+                    elif az > 180:
+                        az = -(360 - az)
+
+                    result, msg = set_position(AZIMUTH_STEPPER, absolute=True, move_degrees=az)
+
+                    if elev > 0:
+                        result, msg = set_position(ELEVATION_STEPPER, absolute=True, move_degrees=elev)
 
                     key = stdscr.getch()
                     if key == 27 or key == ord('q'):
@@ -206,16 +219,16 @@ try:
                     msg = ""
                     if key != -1:
                         if key == curses.KEY_UP:
-                            result, msg = set_position_with_keyboard(ELEVATION_STEPPER, move_degrees=1)
+                            result, msg = set_position(ELEVATION_STEPPER, absolute=False, move_degrees=1)
                             command_description = "up"
                         elif key == curses.KEY_DOWN:
-                            result, msg = set_position_with_keyboard(ELEVATION_STEPPER, move_degrees=-1)
+                            result, msg = set_position(ELEVATION_STEPPER, absolute=False, move_degrees=-1)
                             command_description = "down"
                         elif key == curses.KEY_LEFT:
-                            result, msg = set_position_with_keyboard(AZIMUTH_STEPPER, move_degrees=1)
+                            result, msg = set_position(AZIMUTH_STEPPER, absolute=False, move_degrees=1)
                             command_description = "left"
                         elif key == curses.KEY_RIGHT:
-                            result, msg = set_position_with_keyboard(AZIMUTH_STEPPER, move_degrees=-1)
+                            result, msg = set_position(AZIMUTH_STEPPER, absolute=False, move_degrees=-1)
                             command_description = "right"
                         elif key == 27 or key == ord('q'):
                             break
